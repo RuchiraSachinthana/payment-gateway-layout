15ff02e6aba12689e924a774c96a2c5344085b50
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-12-19 22:32:26 +0530

    updated

--- payment-gateway.php
+++ payment-gateway.php
@@ -67,7 +67,21 @@
                 </div>
                 <div class="inputBox">
                     <span>Exp month :</span>
-                    <input type="text" placeholder="January" required>
+                    <select>
+                            <option selected value=''>--Select Month--</option>
+                            <option  value='1'>Janaury</option>
+                            <option value='2'>February</option>
+                            <option value='3'>March</option>
+                            <option value='4'>April</option>
+                            <option value='5'>May</option>
+                            <option value='6'>June</option>
+                            <option value='7'>July</option>
+                            <option value='8'>August</option>
+                            <option value='9'>September</option>
+                            <option value='10'>October</option>
+                            <option value='11'>November</option>
+                            <option value='12'>December</option>
+                 </select>
                 </div>
 
                 <div class="flex">
