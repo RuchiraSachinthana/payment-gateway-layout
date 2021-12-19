3e3484afd3f6e9ac334dd4ff3ee42f0e87e0a66d
Ruchira Sachinthana <ruchira.sachinthana.1@gmail.com>
2021-12-19 21:22:14 +0530

    updated

--- payment-gateway.php
+++ payment-gateway.php
@@ -1,9 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+
+<link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Payment Gateway</title>
 </head>
 <body>
@@ -12,6 +15,6 @@
 
 
 
-
+<script src="js/main.js"></script>
 </body>
 </html>
